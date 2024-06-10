46567cc2cbca4bb4e9dd284b093a145ba24623ed
Fiston <karekezifiston30@gmail.com>
2024-06-10 11:50:38 +0200

    commit basics

---
+++ React Basics/src/Karyuri.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './cssyose.css'
+
+
+
+const Remy = 45;
+
+
+function Sex() {
+  return (
+    <>
+      <h1>{Remy}</h1>
+    </>
+  )
+}
+
+export default Sex
---
+++ React Basics/src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Sex from './Karyuri'
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Sex/>
+  </React.StrictMode>,
+)
